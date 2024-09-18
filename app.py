11127bb3c14dd39742ba5312e4aaf2926a451cc1
Akash35721 <2004akashchauhan@gmail.com>
2024-09-18 15:57:01 +0530

    ci pipeline v2

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open('data/data_movies.json', 'r') as f:
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Hi! This app is an API, there is no UI ;-) Akash has changed it anf ci cd pipeline is up :)"
+    return "Hi! This app is an API, there is no UI ;-) Akash has changed it anf ci cd pipeline is up :)."
 
 
 @app.route('/discover')
